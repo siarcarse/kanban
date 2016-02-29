7a196cda0a0d744ed00afd3ee063e2fcc03eb57a
Simplicio Carcamo <siarcarse@gmail.com>
2016-02-29 20:59:59 -0300

    Cap 3

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const merge = require('webpack-merge');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event; //Capturo variable pasada a NPM (i.e npm START)
+process.env.BABEL_ENV = TARGET;
+
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
@@ -15,16 +17,39 @@ const common = {
     entry: {
         app: PATHS.app
     },
+    // Add resolve.extensions.
+    // '' is needed to allow imports without an extension.
+    // Note the .'s before extensions as it will fail to match without!!!
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
     },
     module: {
         loaders: [{
-            // Test expects a RegExp! Note the slashes!
-            test: /\.css$/,
-            loaders: ['style', 'css'],
-            // Include accepts either a path or an array of paths.
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            },
+            // Set up jsx. This accepts js too thanks to RegExp
+            {
+                test: /\.jsx?$/,
+                // Enable caching for improved performance during development
+                // It uses default OS directory by default. If you need something
+                // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+                loaders: ['babel?cacheDirectory'],
+                // Parse only app files! Without this it will go through entire project.
+                // In addition to being slow, that will most likely result in an error.
+                include: PATHS.app
+            }
+        ],
+        preLoaders: [{
+            test: /\.jsx?$/,
+            loaders: ['eslint', 'jscs'],
             include: PATHS.app
         }]
     }
@@ -48,12 +73,6 @@ if (TARGET === 'start' || !TARGET) {
             stats: 'errors-only',
 
             // Parse host and port from env so this is easy to customize.
-            //
-            // If you use Vagrant or Cloud9, set
-            // host: process.env.HOST || '0.0.0.0';
-            //
-            // 0.0.0.0 is available to all network devices unlike default
-            // localhost
             host: process.env.HOST,
             port: process.env.PORT
         },
