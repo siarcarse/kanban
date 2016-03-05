6627d01d3985aecc7ce2463f3301326806786302
Simplicio Carcamo <siarcarse@gmail.com>
2016-03-04 21:58:22 -0300

    Final con app cache

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,4 +1,3 @@
-
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import NoteActions from '../actions/NoteActions';
@@ -8,6 +7,21 @@ class NoteStore {
         this.bindActions(NoteActions);
 
         this.notes = [];
+        this.exportPublicMethods({
+            getNotesByIds: this.getNotesByIds.bind(this)
+        });
+    }
+    getNotesByIds(ids) {
+        // 1. Make sure we are operating on an array and
+        // map over the ids
+        // [id, id, id, ...] -> [[Note], [], [Note], ...]
+        return (ids || []).map(
+            // 2. Extract matching notes
+            // [Note, Note, Note] -> [Note, ...] (match) or [] (no match)
+            id => this.notes.filter(note => note.id === id)
+            // 3. Filter out possible empty arrays and get notes
+            // [[Note], [], [Note]] -> [[Note], [Note]] -> [Note, Note]
+        ).filter(a => a.length).map(a => a[0]);
     }
     create(note) {
         const notes = this.notes;
@@ -15,9 +29,9 @@ class NoteStore {
         note.id = uuid.v4();
 
         this.setState({
-            notes: notes.concat(note)
-            //notes: [...notes, { task: 'New task' }]
+            notes: [...notes, note]
         });
+        return note;
     }
     update(updatedNote) {
         const notes = this.notes.map(note => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const AppCachePlugin = require('appcache-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event; //Capturo variable pasada a NPM (i.e npm START)
 process.env.BABEL_ENV = TARGET;
 
@@ -20,20 +24,20 @@ const common = {
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        filename: '[bundle].[chunkhash].js',
+        //filename: 'bundle.js',
+        chunkFilename: '[bundle].[chunkhash].js'
     },
     module: {
         loaders: [{
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: PATHS.app
-            },
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel?cacheDirectory'],
-                include: PATHS.app
-            }
-        ],
+            test: /\.css$/,
+            loaders: ['style', 'css'],
+            include: PATHS.app
+        }, {
+            test: /\.jsx?$/,
+            loaders: ['babel?cacheDirectory'],
+            include: PATHS.app
+        }],
         preLoaders: [{
             test: /\.jsx?$/,
             loaders: ['eslint', 'jscs'],
@@ -62,11 +66,44 @@ if (TARGET === 'start' || !TARGET) {
             new webpack.HotModuleReplacementPlugin(),
             new NpmInstallPlugin({
                 save: true // --save
-            })
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "app",
+                minChunks: Infinity,
+            }),
+            new ManifestPlugin(),
+            new ChunkManifestPlugin({
+                filename: "chunk-manifest.json",
+                manifestVariable: "webpackManifest"
+            }),
+            new webpack.optimize.OccurenceOrderPlugin()
         ]
     });
 }
 
 if (TARGET === 'build') {
-    module.exports = merge(common, {});
+    module.exports = merge(common, {
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'node_modules/html-webpack-template/index.ejs',
+                title: 'Kanban app',
+                appMountId: 'app',
+                inject: false
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "app",
+                minChunks: Infinity,
+            }),
+            new ManifestPlugin(),
+            new ChunkManifestPlugin({
+                filename: "chunk-manifest.json",
+                manifestVariable: "webpackManifest"
+            }),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new AppCachePlugin({
+              settings: ['prefer-online'],
+              output: 'my-manifest.appcache'
+            })
+        ]
+    });
 }
